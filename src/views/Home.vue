6981096becdf02e4e4e2c9dd49cc93bb960b6816
Martien Dermawan Tanama <martiendt@gmail.com>
2018-12-27 13:54:25 +0700

    implement vue table blegoh

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Vue table blegoh
+    <hr>
+    <vue-table v-bind:data="users"></vue-table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import VueTable from 'vue-table-blegoh'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    VueTable
+  },
+  data () {
+    return {
+      users: [
+        {
+            "id": 0,
+            "first_name": "Lorraine",
+            "last_name": "Fields",
+            "gender": "female",
+            "age": 20,
+            "eyeColor": "green",
+            "email": "lorrainefields@grok.com",
+            "balance": "$1,189.15",
+            "phone": "+1 (901) 442-3257",
+        },
+        {
+            "id": 1,
+            "first_name": "Stanton",
+            "last_name": "Hebert",
+            "gender": "male",
+            "age": 28,
+            "eyeColor": "green",
+            "email": "stantonhebert@hopeli.com",
+            "balance": "$3,288.77",
+            "phone": "+1 (841) 405-3690",
+        }
+      ]
+    }
   }
 }
 </script>
